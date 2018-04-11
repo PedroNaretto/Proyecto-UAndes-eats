45f2aacfd88ba5a070712e4fb00e19eef9ad24a1
PedroNaretto <ppnaretto@miuandes.cl>
2018-04-11 17:53:23 -0400

    Veamos si funciona

---
+++ Proyecto UAndes eats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_UAndes_eats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Si puedes leer esto es pq funciona");
+            Console.ReadKey();
+        }
+    }
+}
